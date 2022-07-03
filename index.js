03731733fa61faf13e0130129ee27f379149cd06
nosferatu-bodya <kresanbohdan420@gmail.com>
2022-07-03 15:47:18 +0300

    automate saving

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
+if(localStorage.getItem('str')) {
+    importStr(localStorage.getItem('str'))
+}
+
+document.querySelector('.export').addEventListener('click', () => {
+    const str = toOneStr()
+    navigator.clipboard.writeText(str)
+    localStorage.setItem('str', str)
+
+    alert('copied to clipboard')
+})
+
+document.querySelector('.import').addEventListener('click', () => {
+    const textarea = document.querySelector('.input-str');
+    const str = textarea.value
+    importStr(str)
+    textarea.value = ''
+})
+
 function toOneStr() {
     const textareas = document.querySelectorAll('.day textarea')
     const contents = []
@@ -14,15 +33,3 @@ function importStr(str) {
         t.value = contents[i]
     })
 }
-
-document.querySelector('.export').addEventListener('click', () => {
-    navigator.clipboard.writeText(toOneStr())
-    alert('copied to clipboard')
-})
-
-document.querySelector('.import').addEventListener('click', () => {
-    const textarea = document.querySelector('.input-str');
-    const str = textarea.value
-    importStr(str)
-    textarea.value = ''
-})
